e20e38bc55ccc241ae01ef0406b2fffc8aa4f152
andreribeiro-dev <andreribeiro.dev@gmail.com>
2024-05-22 02:55:06 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,11 @@
-console.log("teste")
+// Seleção de Elementos
+const multiplicationForm = document.querySelector("multiplication-form")
+const numberInput = document.querySelector("number")
+const multiplicationInput = document.querySelector("multiplicator")
+
+
+// Funções
+
+
+
+// Eventos
